7b6a107fbadc52e61c3489ba8e8ed4624b0138b8
Ariane Albuquerque <arianeoalbuquerques@gmail.com>
2023-04-30 18:11:54 -0300

    Atualização

---
+++ PotênciaTechAngularDeveloper/JS/Variaveis e Operadores/aula.js
@@ -0,0 +1,13 @@
+//Declaração de variáveis:
+
+let variavel = 10 + 10; //Váriavel que pode ser alterada
+var nome = "Ariane";
+const eAzul = true;
+const eRosa = false;
+const pi = 3.14; //Declaração de constante
+console.log(variavel); //Imprime no console 
+
+
+// * Estruturas Condicionais: 
+
+
